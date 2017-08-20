50d6c7ee15f179c551c99ce90a8e72488de3b485
kabugoivan <ivannavi20@gmail.com>
2017-08-20 11:31:04 +0300

    Add files via upload

---
+++ JavaFolder/MusicController.java
@@ -0,0 +1,17 @@
+package magojamillah.wolfplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by MagoJamillah on 19/07/2017.
+ */
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
